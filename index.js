1d69ffb3e9d6fa7ad1ecba27ea25a55ee6148c6a
Nicolas9000 <92756075+Nicolas9000@users.noreply.github.com>
2024-11-28 12:11:09 +0100

    carre case

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 module.exports = function computeFiguresPoint(dices) {
   for (let i = 0; i < dices.length; i++) {
     const isBrelan = dices.filter((dice) => dice === dices[i]).length === 3;
+    const isCarre = dices.filter((dice) => dice === dices[i]).length === 4;
     if (isBrelan) {
       return 28;
+    }
+    if (isCarre) {
+      return 35;
     }
   }
   const isYamsFigure = dices.every((dice) => dice === dices[0]);
--- index.test.js
+++ index.test.js
@@ -10,4 +10,7 @@ describe("Figure result", () => {
   it("Brelan figure", () => {
     expect(computeFiguresPoint([1, 1, 1, 4, 5])).toBe(28);
   });
+  it("Carré figure", () => {
+    expect(computeFiguresPoint([1, 1, 1, 1, 5])).toBe(35);
+  });
 });
