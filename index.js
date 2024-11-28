2bbd2839adff877c4ad9993aae6decfbc7d429fc
Nicolas9000 <92756075+Nicolas9000@users.noreply.github.com>
2024-11-28 12:11:55 +0100

    refacto

--- index.js
+++ index.js
@@ -2,16 +2,17 @@ module.exports = function computeFiguresPoint(dices) {
   for (let i = 0; i < dices.length; i++) {
     const isBrelan = dices.filter((dice) => dice === dices[i]).length === 3;
     const isCarre = dices.filter((dice) => dice === dices[i]).length === 4;
+    const isYamsFigure = dices.filter((dice) => dice === dices[i]).length === 5;
     if (isBrelan) {
       return 28;
     }
     if (isCarre) {
       return 35;
     }
+    if (isYamsFigure) {
+      return 50;
+    }
   }
-  const isYamsFigure = dices.every((dice) => dice === dices[0]);
-  if (isYamsFigure) {
-    return 50;
-  }
+
   return dices.reduce((acc, curr) => acc + curr, 0);
 };
